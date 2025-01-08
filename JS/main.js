1945af7f42172991a7dd4b540aa5bcb11efa0866
Yousef Assem <usefassem96@gmail.com>
2025-01-08 19:55:55 +0200

    fixing responsiveness

--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,16 @@
 $(window).scroll(function () {
     let sectionTop = $("#about").offset().top;
-    let Top = $(window).scrollTop()
-    if(Top > sectionTop - 5){
-        $(".navbar-cstm").css("background-color" , "white")
-        $(".ul-cstm li a").addClass("text-black")
-        $("#logoImg").attr("src" , "images/logo-dark.png")
-    } 
-    else{
-        $(".navbar-cstm").css("background-color" , "transparent")
-        $(".ul-cstm li a").removeClass("text-black")
-        $("#logoImg").attr("src" , "images/logo-light.png")
-    }
-})
+    let Top = $(window).scrollTop();
 
-$(".navbar .nav-link").click(function(){
-    let hrefval = $(this).attr("href")
-    let sectionTop = $(hrefval).offset().top;
-    $("html , body").animate({scrollTop : sectionTop} , {duration : 2000 , queue : false })
-})
+    if (Top > sectionTop - 5) {
+        $(".navbar-cstm").css("background-color", "white");
+        $(".ul-cstm li a").addClass("text-black");
+        $("#logoImg").attr("src", "images/logo-dark.png");
+        $(".navbar-toggler i").addClass("toggler-black"); // Add black color to toggler
+    } else {
+        $(".navbar-cstm").css("background-color", "transparent");
+        $(".ul-cstm li a").removeClass("text-black");
+        $("#logoImg").attr("src", "images/logo-light.png");
+        $(".navbar-toggler i").removeClass("toggler-black"); // Revert toggler color to default
+    }
+});
